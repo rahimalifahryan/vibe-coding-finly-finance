ec09ca89a75e5f78715f4d6c20b56b745403fda2
rahimalifahryan <rahimalifahryan@gmail.com>
2026-08-11 10:04:00 +0700

    fix(vite): use main.jsx entrypoint and configure JSX esbuild loader for vite build

--- vite.config.js
+++ vite.config.js
@@ -2,11 +2,23 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react({
+      include: '**/*.{jsx,js}',
+    }),
+  ],
   root: './src',
   esbuild: {
     loader: 'jsx',
-    include: /src\/.*\.js$/,
+    include: /src\/.*\.[jt]sx?$/,
+    exclude: [],
+  },
+  optimizeDeps: {
+    esbuildOptions: {
+      loader: {
+        '.js': 'jsx',
+      },
+    },
   },
   build: {
     outDir: '../dist',
